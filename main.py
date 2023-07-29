0bf44a7b7d8bf0c79743993ccfcbd6c6912fa801
Nguyen Hong Dat <nguyenhongdat09@gmail.com>
2023-07-29 12:06:02 +0700

    ok r

--- main.py
+++ main.py
@@ -6,20 +6,27 @@ entity_pattern = re.compile(r'<!ENTITY\s+(.*?)\s+SYSTEM\s+"(.*?)"\s*>')
 
 # Định nghĩa regex cho tìm kiếm chuỗi trong cặp dấu ngoặc kép ""
 string_pattern = re.compile(r'"(.*?)"')
-
+string_endfile = [".ent", ".xml"]
+file_khong_hop_le = []
+file_hop_le = []
 def list_entity(text):
     matches = entity_pattern.findall(text)
     controllers_path = os.path.dirname(os.path.dirname(path))
     list_invalid, list_valid = [], []
     for entity_name, entity_value in matches:
         file = controllers_path + "\\"  + re.sub(r"\.\.\\", "", entity_value)
-        if entity_value.endswith(".xml") & (not os.path.exists(file)):
-            list_invalid.append(file)
+        if not os.path.exists(file):
+            for end_char in string_endfile:
+                if entity_value.endswith(end_char):
+                    list_invalid.append(file)
         else:
-            list_valid.append(file)
+            for end_char in string_endfile:
+                if entity_value.endswith(end_char):
+                    list_valid.append(file)
     return list_invalid, list_valid
 
-def readXml():
+def readXml(path):
+    global file_khong_hop_le
     if not os.path.exists(path):
         print("Not exists path")
         return
@@ -27,7 +34,9 @@ def readXml():
     with open(path, "r", encoding="utf-8") as file:
         xml_text = file.read()
     list_invalid, list_valid = list_entity(xml_text)
-    for file in list_invalid:
-        print(f"File Name {file}")
+    file_khong_hop_le.extend(list_invalid)
+    for file in list_valid:
+        print(f"Reading: {file}")
 
-readXml()
+readXml(path)
+print(file_khong_hop_le)
